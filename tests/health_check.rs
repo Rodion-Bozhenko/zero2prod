00674e65a9f27c5aad9dcee57160bdc8075beed3
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2024-02-26 16:34:12 +0100

    Add configuration and script for starting and connecting to db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use sqlx::{Connection, PgConnection};
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::test]
 async fn healt_check_works() {
@@ -14,6 +15,67 @@ async fn healt_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let client = reqwest::Client::new();
+
+    let body = "name=porqe%20maria&email=porqe_maria%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "porqe_maria@gmail.com");
+    assert_eq!(saved.name, "porqe maria");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_form_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=porqe%20maria", "missing the email"),
+        ("email=porqe_maria%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when payload was {}.",
+            error_message
+        );
+    }
 }
 
 fn spawn_app() -> String {
